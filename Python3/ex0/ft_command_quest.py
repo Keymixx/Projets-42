23c676eb6a3ecf9798a6282d1322a31e4271dc27
Carl Aubert <caaubert@c1r3p7.42lehavre.fr>
2025-12-11 20:58:23 +0100

    Piton

---
+++ Python3/ex0/ft_command_quest.py
@@ -0,0 +1,17 @@
+import sys
+
+print("=== Command Quest ===")
+len_args = len(sys.argv)
+if len_args > 1:
+	print(f"Program name: {sys.argv[0]}")
+	print(f"Arguments received: {len_args - 1}")
+	i = 1;
+	while i < len_args:
+		print(f"Argument {i}: {sys.argv[i]}")
+		i += 1
+	print(f"Total arguments: {len_args}")
+
+else:
+	print("No arguments provided!")
+	print(f"Program name: {sys.argv[0]}")
+	print(f"Total arguments: {len_args}")
