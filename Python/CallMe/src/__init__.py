bd7574dcc7c905e641264f49ebf66b595c9d53cf
Carl Aubert <caaubert@c1r5p10.42lehavre.fr>
2026-04-08 18:09:33 +0200

    1

--- Python/CallMe/src/__init__.py
+++
@@ -1,7 +0,0 @@
-__author__ = "caaubert"
-__version__ = "1.0"
-
-from llm_sdk import Small_LLM_Model
-from random import randint
-
-__all__ = [Small_LLM_Model, randint]
--- Python/CallMe/src/main.py
+++
@@ -1,9 +0,0 @@
-from . import Small_LLM_Model, randint
-Model = Small_LLM_Model(
-    model_name="Qwen/Qwen3-0.6B",
-    device=None,
-    dtype=None,
-    trust_remote_code=True,
-)
-
-num = randint(1, 10)
